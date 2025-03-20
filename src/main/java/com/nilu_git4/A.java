0ce3b1dfc3f42fafd0fabdd8305e985c3d40b324
nilu <ndebankura01@gmail.com>
2025-03-20 10:36:26 +0530

    lalla

---
+++ src/main/java/com/nilu_git4/A.java
@@ -0,0 +1,8 @@
+package com.nilu_git4;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello, Nilu!");
+        System.out.println("This is my first Java program.");
+    }
+}
---
+++ src/test/java/com/nilu_git4/NiluGit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilu_git4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiluGit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
